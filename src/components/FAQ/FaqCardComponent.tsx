96772a3f6ee0a07c5f6a6cc87dd2bcdb9fe3106f
GINTAS BAZYS <gintas@adisoft.lt>
2024-06-16 20:13:15 +0300

    style fix

--- src/components/FAQ/FaqCardComponent.tsx
+++ src/components/FAQ/FaqCardComponent.tsx
@@ -16,8 +16,8 @@ const FaqCardComponent: React.FC<FaqCardComponentProps> = ({ faqs }) => {
 		<Accordion id="faqAccordion">
 			{faqs.map(({ id, question, answer }) => (
 				<AccordionPanel key={id}>
-					<AccordionTitle className="text-bold text-primary border-b border-primary">{question}</AccordionTitle>
-					<AccordionContent className="pt-0 !border-t-0">
+					<AccordionTitle className="px-0 gap-4 text-bold text-primary border-b border-primary">{question}</AccordionTitle>
+					<AccordionContent className="pt-0 px-0 !border-t-0">
 						<p className="mb-2">{answer}</p>
 					</AccordionContent>
 				</AccordionPanel>
