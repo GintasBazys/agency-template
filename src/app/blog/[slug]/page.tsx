9f60d32e254da63a639afa2771e47ee6b2aeafc1
GINTAS BAZYS <gintas@adisoft.lt>
2024-07-03 10:09:32 +0300

    additional styling

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -3,6 +3,7 @@ import remarkGfm from "remark-gfm";
 import { MDXRemote } from "next-mdx-remote-client/rsc";
 import { MDXRemoteOptions } from "next-mdx-remote-client/rsc";
 import { components } from "@/utils/components";
+import Image from "next/image";
 
 import "./post.css";
 
@@ -28,7 +29,7 @@ export async function generateMetadata({ params }: PostPageProps) {
 
 const PostPage = async ({ params }: PostPageProps) => {
     const { slug } = params;
-    const { content } = await getPostBySlug(slug);
+    const { data, content } = await getPostBySlug(slug);
     const options: MDXRemoteOptions = {
         mdxOptions: {
             remarkPlugins: [remarkGfm],
@@ -38,7 +39,20 @@ const PostPage = async ({ params }: PostPageProps) => {
 
     return (
         <main className="flex flex-col items-center justify-between p-12 px-4 md:px-24">
-            <section className="container max-w-7xl mx-auto px-4 static-page">
+            <section className="container max-w-7xl mx-auto lg:px-4 static-page">
+                <h1 className="text-center !mb-1">{data.title}</h1>
+                <p className="text-center">
+                    By: {data.author} | {data.date}
+                </p>
+                <Image
+                    className="mx-auto mb-8"
+                    src={data.image}
+                    alt={data.title}
+                    title={data.title}
+                    width={500}
+                    height={500}
+                />
+                <hr/>
                 <MDXRemote
                     source={content}
                     options={options}
